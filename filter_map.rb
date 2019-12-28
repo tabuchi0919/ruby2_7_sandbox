4a10723a86de57bafb9b1fc643598489905e1e67
tabuchi-ga-tech <k_tabuchi@ga-tech.co.jp>
2019-12-28 18:58:28 +0900

    add destructive methods

--- filter_map.rb
+++ filter_map.rb
@@ -9,11 +9,14 @@ Benchmark.bmbm do |x|
   x.report('select + map') do
     N.times { range.select(&:even?).map { |i| i + 1 } }
   end
+  x.report('select + map!') do
+    N.times { range.select(&:even?).map! { |i| i + 1 } }
+  end
   x.report('map + compact') do
     N.times { range.map { |i| i + 1 if i.even? }.compact }
   end
-  x.report('dup + map! + compact!') do
-    N.times { range.dup.map! { |i| i + 1 if i.even? }.compact! }
+  x.report('map + compact!') do
+    N.times { range.map { |i| i + 1 if i.even? }.compact! }
   end
   x.report('filter_map') do
     N.times { range.filter_map { |i| i + 1 if i.even? } }
